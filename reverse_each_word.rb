17a1f319fabcd44e6ca687ca3f4105dac4ccb340
keyannamayfield@gmail.com <keyannamayfield@gmail.com>
2018-06-17 19:04:55 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,8 @@
+sentence1 = ["Hello there, and how are you?"]
+
+def reverse_each_word(sentence1)
+  words = sentence1.split(" ").collect do | word |
+    word.reverse
+  end
+    words.join(" ")
+end
