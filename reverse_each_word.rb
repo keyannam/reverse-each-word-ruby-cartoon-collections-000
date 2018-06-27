d12289c287dd1e01096708738ca26e8343f7cda7
keyannamayfield@gmail.com <keyannamayfield@gmail.com>
2018-06-27 15:01:49 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 sentence1 = ["Hello there, and how are you?"]
 
 def reverse_each_word(sentence1)
-  words = sentence1.split(" ").each do | word |
+  words = sentence1.split(" ").collect do | word |
     word.reverse
   end
     words.join(" ")
